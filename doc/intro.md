658b3c7d1727550f3a4f25d4ec7dd1349d48ed82
Victor Jolissaint <victor.jolissaint@gmail.com>
2016-03-26 15:47:30 +0000

    Initial commit app template from lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to discover-weekly-archivist
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/discover_weekly_archivist/core_test.clj
@@ -0,0 +1,7 @@
+(ns discover-weekly-archivist.core-test
+  (:require [clojure.test :refer :all]
+            [discover-weekly-archivist.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
