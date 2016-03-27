f25a4cb55c1e13968c078f9f5c71f5f13506b791
Victor Jolissaint <victor.jolissaint@gmail.com>
2016-03-27 15:33:48 +0100

    add clj-time lib

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.3.3"]
-                 [clj-spotify "0.1.1"]]
+                 [clj-spotify "0.1.1"]
+                 [clj-time "0.11.0"]]
   :main ^:skip-aot discover-weekly-archivist.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
